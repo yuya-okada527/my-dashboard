69f3373d9a52b361cdd2710f99ef99d180758cac
yuya-okada527 <yuya.okada527@gmail.com>
2022-11-06 22:36:53 +0900

    commit all

--- tui/src/main.py
+++ tui/src/main.py
@@ -1,17 +1,29 @@
+import dataclasses
+
 from textual.app import App, ComposeResult, RenderResult
 from textual.widget import Widget
 
 
+@dataclasses.dataclass
+class Command:
+    name: str
+    description: str
+
+
 class CommandWidget(Widget):
 
+    def __init__(self, name: str, description: str) -> None:
+        super().__init__()
+        self.command = Command(name=name, description=description)
+
     def render(self) -> RenderResult:
-        return "CMD-D: Split Split Pane Right"
+        return f"{self.command.name}: {self.command.description}"
 
 
 class Dashboard(App):
 
     def compose(self) -> ComposeResult:
-        yield CommandWidget()
+        yield CommandWidget(name="CMD-D", description="Split Pane Right")
 
 
 if __name__ == "__main__":
