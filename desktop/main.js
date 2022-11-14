f062a54c031ff983b3b1d4c2bff6bfba74d7f4a7
yuya-okada527 <yuya.okada527@gmail.com>
2022-11-14 22:36:32 +0900

    add activate event

--- desktop/main.js
+++ desktop/main.js
@@ -10,8 +10,12 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-// app.on("window-all-closed", () => {
-//   if (process.platform !== "darwin") app.quit();
-// });
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
