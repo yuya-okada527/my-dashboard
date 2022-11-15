481128c8de60025997a0b58428aa87e53da40a94
yuya-okada527 <yuya.okada527@gmail.com>
2022-11-15 23:29:10 +0900

    add ipcMain

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,7 +1,8 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
 });
